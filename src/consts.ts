009ee30222785584e48d138d0b6158b6b0ee2d42
たは <karahaya626@gmail.com>
2026-07-28 23:44:13 +0900

    Update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'taha.log()';
+export const SITE_DESCRIPTION = 'ゲームを中心に、気になったことを気ままに記録する個人ブログ。';
